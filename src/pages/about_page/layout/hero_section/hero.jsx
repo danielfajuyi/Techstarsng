d3dab159d5cd29ef87d3832c0043bd1a56fe5bc2
Daniel Fajuyi <ayomidedaniels121@gmail.com>
2024-10-29 17:44:51 +0100

    add layout to about

---
+++ src/pages/about_page/layout/hero_section/hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./hero.scss";
+
+const Hero = () => {
+  return (
+    <>
+      <header className="hero-section">Hero Section</header>
+    </>
+  );
+};
+
+export default Hero;
---
+++ src/pages/about_page/layout/main_section/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./main.scss";
+import Features from "../../component/features/features";
+
+const Main = () => {
+  return (
+    <>
+      <main>main section</main>
+    </>
+  );
+};
+
+export default Main;
