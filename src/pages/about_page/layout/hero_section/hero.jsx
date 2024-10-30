c540b7b4ac625632835a1841d57c71d3b311d684
kachi <agohdennis99@gmail.com>
2024-10-30 19:23:59 +0100

    About page progress

--- src/pages/about_page/layout/hero_section/hero.jsx
+++ src/pages/about_page/layout/hero_section/hero.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./hero.scss";
+import blogImage from "../../../../assets/images/blogImage.jpg";
 
 const Hero = () => {
   return (
     <>
-      <header className="hero-section">Hero Section</header>
+     <main className="container">
+       <header className="hero-section">
+        <h2>About <span className="u">u</span>s</h2>
+      </header>
+
+      <div className="blog-container">
+        <div className="image-container">
+          <img src={blogImage} className="blog-image" alt="blogimage" />
+        </div>
+        <div className="text">
+          <p>Zealworkers is a freelancing marketplace established in the year 2019, with the aim of bridging the gap between freelancers and potential client in need of skillful and resourceful zealous worker.
+Zealworkers set itself aside from the traditional market place by solving the issue of cross border transaction in digital marketplace and complexity in the user experience by integrating Blockchain technology and simple user interface.
+</p>
+        </div>
+      </div>
+     
+ </main>
+
+
     </>
   );
 };
