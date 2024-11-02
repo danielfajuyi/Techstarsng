99252f6c5bfa00b0033cee5f48ed278c2fdc9d39
Daniel Fajuyi <ayomidedaniels121@gmail.com>
2024-11-02 08:04:44 +0100

    started homepage

--- src/pages/homepage/layout/main_section/main.jsx
+++ src/pages/homepage/layout/main_section/main.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./main.scss";
-import Features from "../../component/features/features";
+import main02 from "../../../../assets/images/main02.png";
 
 const Main = () => {
   return (
     <>
-      <main>main section</main>
+      <main>
+        <div className="main-01">
+          <h4>For Clients</h4>
+          <h2>Hire Professional Talents</h2>
+          <p>
+            Collaborate with an extensive community of skilled freelancers to
+            accomplish tasks—whether it's fast-paced projects or substantial
+            transformations.
+          </p>
+
+          <button> Hire Talents </button>
+        </div>
+        <div className="main-02">
+          <img src={main02} alt="" />
+        </div>
+      </main>
     </>
   );
 };
